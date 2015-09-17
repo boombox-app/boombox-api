1e1508095e953ed20e2d514c159c98047f994210
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-09-17 01:19:22 -0300

    Define a simple structure of project

---
+++ app/index.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/_build'));
+
+app.get('/api', function (req, res) {
+  res.json({message: 'API Working', status: 'success'});
+});
+
+app.get('*', function (req, res) {
+  res.sendfile('./_build/index.html');
+});
+
+app.listen(port);
+
+console.log('Magic happens on port ' + port);
+
+exports = module.exports = app;
