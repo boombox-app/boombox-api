e26f7e926c48e1eb4b0e7733b19d38e5f97e164f
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-09-18 03:03:09 -0300

    Resolve problems with static files

--- app/index.js
+++ app/index.js
@@ -5,14 +5,19 @@ var express = require('express'),
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  console.log(req.url);
+  next();
+})
+console.log(__dirname + '/../_build');
 
-app.use(express.static(__dirname + '/_build'));
+app.use(express.static(__dirname + '/../_build'));
 
 app.get('/api', function (req, res) {
   res.json({message: 'API Working', status: 'success'});
 });
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendfile('./_build/index.html');
 });
 
