72408d029d8bb2b25057024af65ec999186c53c6
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-09-21 15:52:35 -0300

    Add SoundCloud Service

--- app/services/SoundcloudService.js
+++ app/services/SoundcloudService.js
@@ -1,23 +1,24 @@
-// var SC = require('node-soundcloud');
+var SC = require('node-soundcloud'),
+    Promise = require("bluebird");
 
-// SC.init({
-//   id: '10b1e7bf6b21e1a2a2971196b918833a',
-//   secret: 'a24f20b1e7c03f68c52b0dd6f8ac1767',
-//   uri: 'localhost:3000'
-//   // accessToken: 'your existing access token'
-// });
+SC.init({
+  id: '10b1e7bf6b21e1a2a2971196b918833a',
+  secret: 'a24f20b1e7c03f68c52b0dd6f8ac1767',
+  uri: 'localhost:3000'
+});
 
-// var url = SC.getConnectUrl(function () {
-//   console.log("->", arguments);
-// });
+function get(query) {
 
-// console.log("URl->", url);
+  SC.get('/search/?q=' + encodeURI(query), function(err, track) {
+    if ( err ) {
+      throw err;
+      return;
+    }
 
-// // SC.authorize(code, function(err, accessToken) {
-// //   if ( err ) {
-// //     throw err;
-// //   } else {
-// //     // Client is now authorized and able to make API calls
-// //     console.log('access token:', accessToken);
-// //   }
-// // });
+    return track;
+  });
+}
+
+module.exports = {
+  get : get
+};
