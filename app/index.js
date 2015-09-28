8fc7e86db39d8ec7017c952649c6ec33d366337b
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-09-28 09:33:11 -0300

    Enable Cors

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    cors = require('cors'),
     app = express();
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../_build'));
+
+app.use(cors());
 
 app.use('/', require('./routes'));
 
